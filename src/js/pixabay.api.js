8489321021fcc2e30f8753cfd089fe0cada43a82
CodePrincessNastia <145288463+CodePrincessNastia@users.noreply.github.com>
2024-03-10 23:41:14 +0200

    pixabay

--- src/js/pixabay.api.js
+++ src/js/pixabay.api.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = "https://pixabay.com/api/";
+export async function fetchImages(query, currentPage) {
+  const API_KEY = '42789020-c2dc414543595960212044fc8';
+  const BASE_URL = 'https://pixabay.com';
+  const END_POINT = '/api/';
+  const url = `${BASE_URL}${END_POINT}`;
 
-    const params = {
-        key: "42789020-c2dc414543595960212044fc8",
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-        per_page: 15,
-        page: currentPage,
-    };
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    per_page: 15,
+    page: currentPage,
+  };
 
-export async function getData(query, page) {
-    try {
-        const resp = await axios.get('', { params: params });
-        return resp;
-    } catch (error) {
-        console.error(error);
-    }
+  const response = await axios.get(url, { params });
+
+  return response.data;
 }
