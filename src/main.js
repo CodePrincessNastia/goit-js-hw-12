1dd88ee8dd67b5a4d1f7ea3a1705c4546f35d54e
CodePrincessNastia <145288463+CodePrincessNastia@users.noreply.github.com>
2024-03-10 23:10:16 +0200

    library

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import izitoast from 'izitoast';
+import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
